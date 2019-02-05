71fe7d9c6a8713a05672bd3baf160c869930f536
AsenKoparanski <asenakoparanski@gmail.com>
2019-02-05 23:46:25 +0000

    Update Sale.java

--- src/sample/model/Sale.java
+++ src/sample/model/Sale.java
@@ -1,5 +1,8 @@
 package sample.model;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 /**
  * Author: Asen Koparanski
  * Purpose:
@@ -8,40 +11,63 @@ package sample.model;
 
 public class Sale {
 
-    private String description;
-    private String details;
-    private int employeeId;
-    private String date;
+    private SimpleStringProperty description;
+    private SimpleStringProperty details;
+    private SimpleIntegerProperty employeeId;
+    private SimpleStringProperty date;
 
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
+    public Sale() {
+        this.description = new SimpleStringProperty();
+        this.details = new SimpleStringProperty();
+        this.employeeId = new SimpleIntegerProperty();
+        this.date = new SimpleStringProperty();
     }
 
     public String getDescription() {
+        return description.get();
+    }
+
+    public SimpleStringProperty descriptionProperty() {
         return description;
     }
 
     public void setDescription(String description) {
-        this.description = description;
+        this.description.set(description);
     }
 
     public String getDetails() {
+        return details.get();
+    }
+
+    public SimpleStringProperty detailsProperty() {
         return details;
     }
 
     public void setDetails(String details) {
-        this.details = details;
+        this.details.set(details);
     }
 
     public int getEmployeeId() {
+        return employeeId.get();
+    }
+
+    public SimpleIntegerProperty employeeIdProperty() {
         return employeeId;
     }
 
     public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
+        this.employeeId.set(employeeId);
+    }
+
+    public String getDate() {
+        return date.get();
+    }
+
+    public SimpleStringProperty dateProperty() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date.set(date);
     }
 }
