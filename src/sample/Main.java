71c842bef48ae8989b48059b34a340065b82fa98
AsenKoparanski <asenakoparanski@gmail.com>
2019-02-07 16:53:14 +0000

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader =new FXMLLoader(getClass().getResource("main.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
         Parent root = loader.load();
         Controller controller = loader.getController();
         controller.listEmployees();
@@ -37,11 +37,11 @@ public class Main extends Application {
             System.out.println("FATAL ERROR: Couldn't connect to database");
             Platform.exit();
         }
-        try {
-            Datasource.getInstance().insertEmployee(138,"Halil");
-        } catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+//        try {
+//            Datasource.getInstance().insertEmployee(138,"Halil");
+//        } catch (SQLException e) {
+//            System.out.println("Error: " + e.getMessage());
+//        }
     }
 
     @Override
