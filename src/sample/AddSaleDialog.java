ef36db0ed5669327bd2cb6fa9ae6d428a800c96c
AsenKoparanski <asenakoparanski@gmail.com>
2019-02-16 19:04:37 +0000

    Update AddSaleDialog.java

--- src/sample/AddSaleDialog.java
+++ src/sample/AddSaleDialog.java
@@ -1,4 +1,48 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import sample.model.Datasource;
+import sample.model.Employee;
+import sample.model.Sale;
+
+import java.sql.SQLException;
+import java.time.format.DateTimeFormatter;
+
 public class AddSaleDialog {
+
+    @FXML
+    private TextField descriptionField;
+
+    @FXML
+    private TextArea detailsArea;
+
+    @FXML
+    private DatePicker saleDatePicker;
+
+    public Sale addSale(Employee emp) {
+
+        String description = descriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        String saleDate = saleDatePicker.getValue().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+
+        Sale sale = new Sale();
+        sale.setDescription(description);
+        sale.setDetails(details);
+        sale.setDate(saleDate);
+        sale.setEmpId(emp.getId());
+
+        // check if null
+        // check if existing ID, send appropriate prompt message
+        try {
+            sale.setId(Datasource.getInstance().highestSalesId());
+            Datasource.getInstance().insertSale(sale);
+        } catch (SQLException e) {
+            System.out.println("Failed to insert sale in database: " + e.getMessage());
+            return null;
+        }
+        return sale;
+    }
 }
