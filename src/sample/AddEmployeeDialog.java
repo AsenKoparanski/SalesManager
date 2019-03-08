0779d9f348aee29b02c5fa35c518a8102e52acf4
AsenKoparanski <asenakoparanski@gmail.com>
2019-03-08 00:09:32 +0000

    Update AddEmployeeDialog.java

--- src/sample/AddEmployeeDialog.java
+++ src/sample/AddEmployeeDialog.java
@@ -20,24 +20,17 @@ public class AddEmployeeDialog {
         String idToInt = empId.getText().trim();
         int employeeId = Integer.parseInt(idToInt);
         String employeeName = empName.getText().trim();
+
         if (employeeId > 0 && employeeName != null) {
             Employee emp = new Employee();
             emp.setId(employeeId);
             emp.setName(employeeName);
             // check if null
             // check if existing ID, send appropriate prompt message
-            try {
-                if (Datasource.getInstance().insertEmployee(emp)) {
-                    return emp;
-                } else {
-                    return null;
-                }
-            } catch (SQLException e) {
-                System.out.println("Failed to insert employee in database: " + e.getMessage());
-                return null;
-            }
+        return emp;
         }
         return null;
+
     }
 }
 
